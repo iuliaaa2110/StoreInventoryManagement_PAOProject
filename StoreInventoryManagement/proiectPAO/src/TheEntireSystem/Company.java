5ea2ae357cc9491ec16cc96c3c8fd00be4688ebd
iuliaaa2110 <iulia_andreea9@yahoo.com>
2021-04-02 12:52:37 +0300

    made Company's attributes private

--- StoreInventoryManagement/proiectPAO/src/TheEntireSystem/Company.java
+++ StoreInventoryManagement/proiectPAO/src/TheEntireSystem/Company.java
@@ -1,20 +1,18 @@
 package TheEntireSystem;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 // for the moment this class is just an example. It has default values to test the project on
 public class Company {
-    ArrayList<Store> franchisePoints;
-    StoreHouse storeHouse;
+    private ArrayList<Store> franchisePoints;
+    private StoreHouse storeHouse;
 
-    ArrayList<Provider> providers;
-    ArrayList<Product> products;
+    private ArrayList<Provider> providers;
+    private ArrayList<Product> products;
 
 
-    public Company() {
+    protected Company() {
 
         Product p0 = new Product("Mozzarella");     // 0
         Product p1 = new Product("Cheese");         // 1
@@ -186,7 +184,7 @@ public class Company {
             }
         })));
 
-        // initialize Store 3 (Supermarket)
+        // initialize Store 4 (Supermarket)
         franchisePoints.get(4).setStoreStock(new StockManagement((new HashMap<Product, Integer>(){
             {
                 put(products.get(11),20);
@@ -204,4 +202,196 @@ public class Company {
         })));
 
     }
+
+
+    //   Show actual stock from the storeHouse
+    protected void Service1 (){
+        System.out.println("Actual stock of the storeHouse is " + storeHouse.mainStock.getStock() + " . \n");
+    }
+
+    //  Show actual stock of a store
+    protected void Service2(){
+        System.out.println("Write the number of the store for which you wanna see the stock.\n");
+
+        Scanner keyboard = new Scanner(System.in);
+        int nr = keyboard.nextInt();
+
+        System.out.println("You chose " + franchisePoints.get(nr).getAddress() + " store");
+        System.out.println("Actual stock of the store" +  franchisePoints.get(nr).getAddress() +"is " + franchisePoints.get(nr).storeStock.getStock() + " .\n");
+    }
+
+    // Show the bank's gain at the storeHouse
+    protected void Service3(){
+
+        System.out.println("StoreHouse's bank = " + storeHouse.getMainBank());
+
+    }
+
+    // Show the bank's gain of a store
+    protected void Service4(){
+        System.out.println("Write the number of the store for which you wanna see the bank's money.");
+
+        Scanner keyboard = new Scanner(System.in);
+        int nr = keyboard.nextInt();
+
+        System.out.println("The amount of money at the store right now is: " + franchisePoints.get(nr).getStoreBank() + " . \n");
+    }
+
+    // Send the money from a store's bank to the StoreHouse's bank
+    protected void Service5(){
+
+        System.out.println("\nWrite the number of the store who needs to send the money to the StoreHouse.");
+
+        Scanner keyboard = new Scanner(System.in);
+        int nr = keyboard.nextInt();
+
+        System.out.println("StoreHouse's bank before: " + storeHouse.getMainBank());
+
+        franchisePoints.get(nr).clearBank();
+
+        System.out.println("StoreHouse's bank now: " + storeHouse.getMainBank());
+
+    }
+
+    // Refill the stock at a store for specific products (with the regular quantity or with a specific quantity)
+    protected void Service6(){
+
+        System.out.println("\nWrite the number of the store who needs the stock refill, the number of the product you need to add, and optional: the quantity. ");
+        System.out.println("With spaces between!");
+        System.out.println("You can do that all over again. Just press '.' if you re done.\n");
+
+        Scanner keyboard = new Scanner(System.in);
+        String st = keyboard.nextLine();
+
+        while(!st.equals(".")){
+
+            String[] t = st.split(" ");
+
+            if(t.length == 2) {
+                int nr = Integer.parseInt(t[0]);
+                int nr2 = Integer.parseInt(t[1]);
+
+                System.out.println(nr + nr2);
+
+                franchisePoints.get(nr).refillProductStock(products.get(nr2));
+            }
+            else if(t.length == 3){
+                int nr = Integer.parseInt(t[0]);
+                int nr2 = Integer.parseInt(t[1]);
+                int nr3 = Integer.parseInt(t[2]);
+
+                franchisePoints.get(nr).refillProductStock(products.get(nr2), nr3);
+            }
+
+            st = keyboard.nextLine();
+        }
+    }
+
+
+    // cand nu mai avem in depozit un produs si vrem sa comandam de la un furnizor,
+    // iteram prin ofertele furnizorilor si il alegem pe cel care vinde produsul mai ieftin
+    // find the provider with the best price for a product
+    protected void Service7() {
+        System.out.println("Write the number of the product you would want to order.");
+
+        Scanner keyboard = new Scanner(System.in);
+        int nr = keyboard.nextInt();
+
+        Product p = products.get(nr);
+
+        PriorityQueue<Map.Entry<BigDecimal, Provider>> pq = new PriorityQueue<>(Map.Entry.comparingByKey());
+
+        for (Provider provider : providers)
+            if (!provider.getPrice(p).equals(new BigDecimal(0)))
+                pq.offer(new AbstractMap.SimpleEntry<>(provider.getPrice(p), provider));
+
+        if (pq.isEmpty())
+            System.out.println("There are no such things in the whole country!! everyone is sold out on this product");
+        else {
+            assert pq.peek() != null;
+
+            Provider chooseProvider = pq.peek().getValue();
+
+            System.out.println(chooseProvider.getName());
+        }
+    }
+
+
+    // make an order to a provider
+    protected void Service8(){
+        System.out.println();
+        System.out.println("Write the number of the provider you wanna buy from");
+
+        Scanner keyboard = new Scanner(System.in);
+        int nr = keyboard.nextInt();
+
+        System.out.println("Write the number of the product you wanna order");
+
+        int nr1 = keyboard.nextInt();
+
+        System.out.println("Write the quantity you need");
+
+        int nr2 = keyboard.nextInt();
+
+        storeHouse.provide(providers.get(nr), products.get(nr1), nr2);
+
+    }
+
+    // make a sell
+    // if the client buys a significantly amount of items from a Supermarket he gets a discount
+    protected void Service9(){
+
+        System.out.println("Type the number of the store where the sell occurs");
+
+        Scanner keyboard = new Scanner(System.in);
+        String st = keyboard.nextLine().trim();
+
+        // stiu ca pare o rezolvare foarte odd,
+        // dar din nush ce motiv cu nextInt imi dadea eroare java.lang.NumberFormatException: For input string: ""
+        String[] t =  st.split(" ");
+        int nr = Integer.parseInt(t[0]);
+
+        if(nr > franchisePoints.size() || st.length() > 1)
+            System.out.println("There is no store with this number/Wrong input.");
+        else {
+
+            Map<Product, Integer> myMap = new HashMap<>();
+
+            System.out.println("And then type productNumber and quantity with space between." +
+                    " Do it how many times you need ( one pair at a line ). Type '.' if you re done.");
+
+            st = keyboard.nextLine().trim();
+            //        st = keyboard.nextLine();
+            //        int Q = Integer.parseInt(tempStr.trim());
+
+            while (!st.equals(".")) {
+                t = st.split(" ");
+                int a = Integer.parseInt(t[0]);
+                Integer b = Integer.parseInt(t[1]);
+
+                myMap.put(products.get(a), b);
+
+                st = keyboard.nextLine().trim();
+            }
+
+            franchisePoints.get(nr).variedSell(myMap);
+        }
+
+    }
+
+    //see the entry price and the out price of a product
+    protected void Service10(){
+
+        System.out.println("Type the number of the product");
+
+        Scanner keyboard = new Scanner(System.in);
+        int nr = keyboard.nextInt();
+
+        if(products.get(nr).getProviderPrice() == null)
+            System.out.println("First you need to make an order for this product. Do it at Service 8 ");
+        else
+            System.out.println("entryPrice = " + products.get(nr).getProviderPrice() + "\noutPrice = "
+                    + products.get(nr).getStorePrice());
+    }
+
 }
--- StoreInventoryManagement/proiectPAO/src/TheEntireSystem/Service.java
+++ StoreInventoryManagement/proiectPAO/src/TheEntireSystem/Service.java
@@ -11,85 +11,37 @@ public class Service {
 
     //   Show actual stock from the storeHouse
     public void Service1 (){
-        System.out.println("Actual stock of the storeHouse is " + company.storeHouse.mainStock.getStock() + " . \n");
+        company.Service1();
     }
 
     //  Show actual stock of a store
     public void Service2(){
-        System.out.println("Write the number of the store for which you wanna see the stock.\n");
-
-        Scanner keyboard = new Scanner(System.in);
-        int nr = keyboard.nextInt();
-
-        System.out.println("You chose " + company.franchisePoints.get(nr).getAddress() + " store");
-        System.out.println("Actual stock of the store is " + company.franchisePoints.get(nr).storeStock.getStock() + " .\n");
+        company.Service2();
     }
 
     // Show the bank's gain at the company.storeHouse
     public void Service3(){
 
-        System.out.println("StoreHouse's bank = " + company.storeHouse.getMainBank());
+        company.Service3();
 
     }
 
     // Show the bank's gain of a store
     public void Service4(){
-        System.out.println("Write the number of the store for which you wanna see the bank's money.");
-
-        Scanner keyboard = new Scanner(System.in);
-        int nr = keyboard.nextInt();
-
-        System.out.println("The amount of money at the store right now is: " + company.franchisePoints.get(nr).getStoreBank() + " . \n");
+        company.Service4();
     }
 
     // Send the money from a store's bank to the StoreHouse's bank
     public void Service5(){
 
-        System.out.println("\nWrite the number of the store who needs to send the money to the StoreHouse.");
-
-        Scanner keyboard = new Scanner(System.in);
-        int nr = keyboard.nextInt();
-
-        System.out.println("StoreHouse's bank before: " + company.storeHouse.getMainBank());
-
-        company.franchisePoints.get(nr).clearBank();
-
-        System.out.println("StoreHouse's bank now: " + company.storeHouse.getMainBank());
+        company.Service5();
 
     }
 
     // Refill the stock at a store for specific company.products (with the regular quantity or with a specific quantity)
     public void Service6(){
 
-        System.out.println("\nWrite the number of the store who needs the stock refill, the number of the product you need to add, and optional: the quantity. ");
-        System.out.println("With spaces between!");
-        System.out.println("You can do that all over again. Just press '.' if you re done.\n");
-
-        Scanner keyboard = new Scanner(System.in);
-        String st = keyboard.nextLine();
-
-        while(!st.equals(".")){
-
-            String[] t = st.split(" ");
-
-            if(t.length == 2) {
-                int nr = Integer.parseInt(t[0]);
-                int nr2 = Integer.parseInt(t[1]);
-
-                System.out.println(nr + nr2);
-
-                company.franchisePoints.get(nr).refillProductStock(company.products.get(nr2));
-            }
-            else if(t.length == 3){
-                int nr = Integer.parseInt(t[0]);
-                int nr2 = Integer.parseInt(t[1]);
-                int nr3 = Integer.parseInt(t[2]);
-
-                company.franchisePoints.get(nr).refillProductStock(company.products.get(nr2), nr3);
-            }
-
-            st = keyboard.nextLine();
-        }
+        company.Service6();
     }
 
 
@@ -97,48 +49,13 @@ public class Service {
     // iteram prin ofertele furnizorilor si il alegem pe cel care vinde produsul mai ieftin
     // find the provider with the best price for a product
     public void Service7() {
-        System.out.println("Write the number of the product you would want to order.");
-
-        Scanner keyboard = new Scanner(System.in);
-        int nr = keyboard.nextInt();
-
-        Product p = company.products.get(nr);
-
-        PriorityQueue<Map.Entry<BigDecimal, Provider>> pq = new PriorityQueue<>(Map.Entry.comparingByKey());
-
-        for (Provider provider : company.providers)
-            if (!provider.getPrice(p).equals(new BigDecimal(0)))
-                pq.offer(new AbstractMap.SimpleEntry<>(provider.getPrice(p), provider));
-
-        if (pq.isEmpty())
-            System.out.println("There are no such things in the whole country!! everyone is sold out on this product");
-        else {
-            assert pq.peek() != null;
-
-            Provider chooseProvider = pq.peek().getValue();
-
-            System.out.println(chooseProvider.getName());
-        }
+        company.Service7();
     }
 
 
     // make an order to a provider
     public void Service8(){
-        System.out.println();
-        System.out.println("Write the number of the provider you wanna buy from");
-
-        Scanner keyboard = new Scanner(System.in);
-        int nr = keyboard.nextInt();
-
-        System.out.println("Write the number of the product you wanna order");
-
-        int nr1 = keyboard.nextInt();
-
-        System.out.println("Write the quantity you need");
-
-        int nr2 = keyboard.nextInt();
-
-        company.storeHouse.provide(company.providers.get(nr), company.products.get(nr1), nr2);
+        company.Service8();
 
     }
 
@@ -146,57 +63,14 @@ public class Service {
     // if the client buys a significantly amount of items from a Supermarket he gets a discount
     public void Service9(){
 
-        System.out.println("Type the number of the store where the sell occurs");
-
-        Scanner keyboard = new Scanner(System.in);
-        String st = keyboard.nextLine().trim();
-
-        // stiu ca pare o rezolvare foarte odd,
-        // dar din nush ce motiv cu nextInt imi dadea eroare java.lang.NumberFormatException: For input string: ""
-        String[] t =  st.split(" ");
-        int nr = Integer.parseInt(t[0]);
-
-        if(nr > company.franchisePoints.size() || st.length() > 1)
-            System.out.println("There is no store with this number/Wrong input.");
-        else {
-
-            Map<Product, Integer> myMap = new HashMap<>();
-
-            System.out.println("And then type productNumber and quantity with space between." +
-                    " Do it how many times you need ( one pair at a line ). Type '.' if you re done.");
-
-            st = keyboard.nextLine().trim();
-            //        st = keyboard.nextLine();
-            //        int Q = Integer.parseInt(tempStr.trim());
-
-            while (!st.equals(".")) {
-                t = st.split(" ");
-                int a = Integer.parseInt(t[0]);
-                Integer b = Integer.parseInt(t[1]);
-
-                myMap.put(company.products.get(a), b);
-
-                st = keyboard.nextLine().trim();
-            }
-
-            company.franchisePoints.get(nr).variedSell(myMap);
-        }
+        company.Service9();
 
     }
 
     //see the entry price and the out price of a product
     public void Service10(){
 
-        System.out.println("Type the number of the product");
-
-        Scanner keyboard = new Scanner(System.in);
-        int nr = keyboard.nextInt();
-
-        if(company.products.get(nr).getProviderPrice() == null)
-            System.out.println("First you need to make an order for this product. Do it at Service 8 ");
-        else
-            System.out.println("entryPrice = " + company.products.get(nr).getProviderPrice() + "\noutPrice = "
-                    + company.products.get(nr).getStorePrice());
+        company.Service10();
     }
 
     public void getService() {
